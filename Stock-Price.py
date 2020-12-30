b8b8d60d7914d6f61a8875b4bd837657556b8854
Noah Krill <51893284+NoahKrill11@users.noreply.github.com>
2020-12-29 21:25:07 -0500

    update to make stocks output to file

--- Stock-Price.py
+++ Stock-Price.py
@@ -1,15 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
-def scrape(url) :
+def scrape(url, file) :
 		response = requests.get(url)
 		soup = BeautifulSoup(response.text, 'lxml')
 		for heading in soup.find_all(["h1"]):
 			name =(heading.text.strip())
 		price = soup.find_all('div',{'class':'D(ib) Mend(20px)'})[0].find('span').text
 		print(name, "$", price)
+		file.write(name +" $ "+ price +'\n')
 	
 url = 'https://finance.yahoo.com/quote/'
 
 inputStock = input("Enter the stock you are looking for ") 
 url+= inputStock
-scrape(url)
+file = open("StockOutput.txt", "a+")
+scrape(url, file)
+
